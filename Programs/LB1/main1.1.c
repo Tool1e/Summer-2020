857c8b9637fe4095cfcb8ab70af142553e7fc60e
Tool1e <68532474+Tool1e@users.noreply.github.com>
2020-08-06 21:38:10 +0300

    Add files via upload

---
+++ Programs/LB1/main1.1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+int main(int argc, char *argv[]) {
+float x,y;
+printf("Vvedit koord tocky(x,y)\n");
+scanf("%f%f",&x,&y);
+if((x>=-2)&&(x<=2)&&(y>=0)&&(y<=2))
+{if (((x*y)+(x*y))*2);
+printf("Tocka vhodyt v vydilenu obl");}
+else
+printf("Tocka ne vhodyt v vydilenu obl");
+return 0;}
+
